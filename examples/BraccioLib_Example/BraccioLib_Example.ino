807554aaae7426d6345b28ce6fa3714c8b0092c7
Jakob Krakauer <janjakob.krakauer@gmail.com>
2026-05-28 11:13:43 +0200

    WIP: BraccioLib_Example experimental testing

--- examples/BraccioLib_Example/BraccioLib_Example.ino
+++ examples/BraccioLib_Example/BraccioLib_Example.ino
@@ -19,32 +19,14 @@ void setup() {
 
   BraccioController robot;
 
-  
-  robot.moveToPosition(550, 0, 50);
-  robot.moveToPosition(550, 0, 280);
-  robot.moveToPosition(550, 0, 50);
-  robot.moveToPosition(550, 0, 30);
-
-
-
-
-  // Braccio.begin();
-
-  // Braccio.ServoMovement(10, 90, 90, 90, 90, 97, 60);
-  // Braccio.ServoMovement(20, 98, 90, 0, 170, 97, 10);
-  // Braccio.ServoMovement(20, 98, 25, 0, 170, 97, 10);
-  // Braccio.ServoMovement(20, 98, 15, 25, 140, 97, 15);
-  // Braccio.ServoMovement(20, 98, 15, 25, 140, 97, 73);
-  // Braccio.ServoMovement(30, 98, 35, 45, 80, 97, 73);
-  // Braccio.ServoMovement(30, 98, 90, 90, 0, 97, 73);
-  // Braccio.ServoMovement(5, 180, 90, 90, 0, 97, 73);
-  // Braccio.ServoMovement(30, 180, 65, 75, 80, 97, 73);
-  // Braccio.ServoMovement(30, 180, 35, 45, 140, 97, 73);
-  // Braccio.ServoMovement(30, 180, 15, 25, 140, 97, 73);
-  // delay(200);
-  // Braccio.ServoMovement(10, 180, 15, 25, 140, 97, 15);
-  // Braccio.ServoMovement(30, 180, 90, 90, 90, 97, 60);
-  // Braccio.ServoMovement(10, 90, 90, 90, 90, 97, 60);
+  // robot.gripperOpen();
+  // robot.moveToPosition(550, 0, 50);
+  // robot.gripperClose();
+  // robot.moveToPosition(550, 0, 280);
+  // robot.moveToPosition(550, 0, 50);
+  // robot.gripperOpen();
+  // robot.moveToPosition(650, 0, 50);
+  robot.moveToPosition(550, 0, 10);
 }
 
 void loop() {
