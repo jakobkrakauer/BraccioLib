e28df4bcd558ebd572cc08a9be76b5e8a685118f
Jakob Krakauer <janjakob.krakauer@gmail.com>
2026-05-28 10:42:08 +0200

    Add GPL v3 copyright headers for cgxeiji/CGx-InverseK

--- src/BraccioController.cpp
+++ src/BraccioController.cpp
@@ -1,3 +1,21 @@
+/*
+ * BraccioLib - BraccioController.cpp
+ * Copyright (C) 2025 Jakob Krakauer
+ *
+ * Contains code based on CGx-InverseK
+ * Copyright (C) 2017 Eiji Onchi
+ * https://github.com/cgxeiji/CGx-InverseK
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
 #include "BraccioController.h"
 
 #include <Arduino.h>
